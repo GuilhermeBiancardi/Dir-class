06ecec13cd96ec1316a2c503b0c77e3a43cd5435
GuilhermeBiancardi <guilhermeaw.com@gmail.com>
2020-01-03 16:41:31 -0300

    Update

--- Dir.class.php
+++ Dir.class.php
@@ -1,1 +1,152 @@
+<?php
 
+class Dir {
+
+    /**
+     * Retorna o diretório atual
+     * @return type
+     */
+    public function diretorioAtual() {
+        return __DIR__;
+    }
+
+    /**
+     * listFileFolder
+     *
+     * @param  mixed $diretorio
+     *
+     * @return void
+     */
+    private function listFileFolder($diretorio) {
+        $arq = array();
+        foreach ($diretorio as $file) {
+            if (!$file->isDot() && $file->isDir()) {
+                $dname = $file->getFilename();
+                $arq[] = $dname;
+            }
+        }
+        foreach ($diretorio as $file) {
+            if ($file->isFile()) {
+                $dname = $file->getFilename();
+                $arq[] = $dname;
+            }
+        }
+        return $arq;
+    }
+
+    /**
+     * listFileOnly
+     *
+     * @param  mixed $diretorio
+     *
+     * @return void
+     */
+    private function listFileOnly($diretorio) {
+        $arq = array();
+        foreach ($diretorio as $file) {
+            if ($file->isFile()) {
+                $dname = $file->getFilename();
+                $arq[] = $dname;
+            }
+        }
+        return $arq;
+    }
+
+    /**
+     * listFolderOnly
+     *
+     * @param  mixed $diretorio
+     *
+     * @return void
+     */
+    private function listFolderOnly($diretorio) {
+        $arq = array();
+        foreach ($diretorio as $file) {
+            if (!$file->isDot() && $file->isDir()) {
+                $dname = $file->getFilename();
+                $arq[] = $dname;
+            }
+        }
+        return $arq;
+    }
+
+    /**
+     * listFileFolderSubFolder
+     *
+     * @param  mixed $diretorio
+     * @param  mixed $diretorio_base
+     *
+     * @return void
+     */
+    private function listFileFolderSubFolder($diretorio, $diretorio_base) {
+        $arq = array();
+        foreach ($diretorio as $file) {
+            if (!$file->isDot() && $file->isDir()) {
+                $dname = $file->getFilename();
+                $retorno = self::listMethod($diretorio_base . '/' . $dname, 2);
+                is_array($retorno) ? $arq[$dname] = $retorno : $arq[$dname] = array();
+            }
+        }
+        foreach ($diretorio as $file) {
+            if ($file->isFile()) {
+                $dname = $file->getFilename();
+                $arq[] = $dname;
+            }
+        }
+        return $arq;
+    }
+
+    /**
+     * listType
+     *
+     * @param  mixed $diretorio
+     * @param  mixed $tipo
+     *
+     * @return void
+     */
+    private function listType($diretorio, $tipo) {
+        $arq = "";
+        if ($tipo == "LIST_ALL") {
+            // Listo Pastas e Arquivos
+            $arq = self::listFileFolder($diretorio);
+        } else if ($tipo == "FILE_ONLY") {
+            // Listo Apenas Arquivos
+            $arq = self::listFileOnly($diretorio);
+        } else if ($tipo == "FOLDER_ONLY") {
+            // Listo Apenas Pastas
+            $arq = self::listFolderOnly($diretorio);
+        }
+        return $arq;
+    }
+
+    /**
+     * $nivel = THIS_FOLDER (Lista apenas a pasta atual)
+     * $nivel = SUB_FOLDERS (Lista o diretorio atual + as sub pastas)
+     * 
+     * $tipo = FILE_ONLY (Lista apenas arquivos)
+     * $tipo = FOLDER_ONLY (Lista apenas pastas)
+     * $tipo = LIST_ALL (Lista os 2)
+     * 
+     * @param DirectoryIterator $diretorio_base
+     * @param string $nivel
+     * @param string $tipo
+     * @return array
+     */
+    public function listMethod($diretorio_base, $nivel = "THIS_FOLDER", $tipo = "LIST_ALL") {
+        if (is_dir($diretorio_base)) {
+            $diretorio = new DirectoryIterator($diretorio_base);
+            if ($nivel == "THIS_FOLDER") {
+                $arq = self::listType($diretorio, $tipo);
+            } else {
+                $arq = self::listFileFolderSubFolder($diretorio, $diretorio_base);
+            }
+            $dir = $arq;
+            return $dir;
+        } else {
+            return "O Diretorio Informado nao Existe";
+        }
+    }
+
+}
+
+?>
--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# Dir-class
+# Dir-Class
+
+## Inclusão da Classe
+
+```php
+include_once "diretorio/Dir.class.php";
+```
+
+## Chamada da Classe
+
+```php
+$dir = new Dir();
+```
+
+### Listando diretórios
+
+Define o PATH para a listagem com as configurações padrões.
+
+```php
+$dados = $dir->listMethod("/PATH/");
+```
+
+Após informar o PATH desejado para ser listado, pode-se informar como segundo parâmetro 2 tipos de listagem:
+
+Parâmetro   | Descrição
+------------|--------------------------------------------------
+THIS_FOLDER | Lista apenas o conteúdo da pasta atual
+SUB_FOLDERS | Lista o conteúdo da pasta atual e suas sub-pastas
+
+```php
+$dados = $dir->listMethod("/PATH/", "TIPO_DE_LISTAGEM");
+```
+Como padrão a listagem vem configurada para mostrar arquivos e pastas, mas você pode mudar isso no terceiro parâmetro (chamado Modo de Listagem):
+
+Parâmetro   | Descrição
+------------|--------------------------------------------------
+FILE_ONLY   | Lista apenas arquivos
+FOLDER_ONLY | Lista apenas pastas
+LIST_ALL    | Lista arquivos e pastas
+
+```php
+$dados = $dir->listMethod("/PATH/", "TIPO_DE_LISTAGEM", "MODO_DE_LISTAGEM");
+```
+
+O retorno contido em **$dados** será um array com as informações da listagem:
+
+```php
+Array(
+	["PASTA_PAI"] => Array(
+		[0] => "ARQUIVO1.txt",
+		[1] => "ARQUIVO2.txt",
+		[2] => "ARQUIVO3.txt"
+	),
+	["PASTA_IRMA"] => Array(
+		["PASTA_FILHA"] => Array(
+			[0] => "ARQUIVO4.txt"
+		)
+	),
+);
+```
+
+Aproveitem!!!
