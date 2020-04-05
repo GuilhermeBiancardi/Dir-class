fd6a8e1f4b2a5786fe6cd7b39b8fee15043e84a9
Guilherme Biacardi <guilhermeaw.com@gmail.com>
2020-04-04 23:24:52 -0300

    Update Diretorio.class.php

--- Diretorio.class.php
+++ Diretorio.class.php
@@ -50,7 +50,6 @@ class Diretorio {
     }
 
     private function listFileFolderSubFolder($diretorio, $diretorio_base) {
-        //$arq = array();
         foreach ($diretorio as $file) {
             if (!$file->isDot() && $file->isDir()) {
                 $dname = $file->getFilename();
@@ -69,13 +68,13 @@ class Diretorio {
 
     private function listType($diretorio, $tipo) {
         $arq = "";
-        if ($tipo == 3) {
+        if ($tipo == "LIST_ALL") {
             // Listo Pastas e Arquivos
             $arq = $this->listFileFolder($diretorio);
-        } else if ($tipo == 1) {
+        } else if ($tipo == "FILE_ONLY") {
             // Listo Apenas Arquivos
             $arq = $this->listFileOnly($diretorio);
-        } else if ($tipo == 2) {
+        } else if ($tipo == "FOLDER_ONLY") {
             // Listo Apenas Pastas
             $arq = $this->listFolderOnly($diretorio);
         }
@@ -83,15 +82,19 @@ class Diretorio {
     }
 
     /**
-     * $nivel = 1 (Lista apenas a pasta atual) $nivel = 2 (Lista o diretorio atual + as sub pastas)
-     * $tipo = 1 (Lista apenas arquivos) $tipo = 2 (Lista apenas pastas) $tipo = 3 (Lista os 2)
+     * $nivel = THIS_FOLDER (Lista apenas a pasta atual)
+     * $nivel = SUB_FOLDERS (Lista o diretorio atual + as sub pastas)
      * 
+     * $tipo = FILE_ONLY (Lista apenas arquivos)
+     * $tipo = FOLDER_ONLY (Lista apenas pastas)
+     * $tipo = LIST_ALL (Lista os 2)
+     *
      * @param DirectoryIterator $diretorio_base
      * @param int $nivel
      * @param int $tipo
      * @return array
      */
-    public function listMethod($diretorio_base, $nivel = 1, $tipo = 3) {
+    public function listMethod($diretorio_base, $nivel = "THIS_FOLDER", $tipo = "LIST_ALL") {
         if (is_dir($diretorio_base)) {
             $diretorio = new DirectoryIterator($diretorio_base);
             if ($nivel == 1) {
