89a663aa6b64d584e6901f0ebb1868339ca03bbf
Guilherme Biacardi <guilhermeaw.com@gmail.com>
2020-04-04 23:18:23 -0300

    Update and rename Dir.class.php to Diretorio.class.php

--- Diretorio.class.php
+++ Diretorio.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class Dir {
+class Diretorio {
 
     /**
      * Retorna o diretório atual
@@ -10,13 +10,6 @@ class Dir {
         return __DIR__;
     }
 
-    /**
-     * listFileFolder
-     *
-     * @param  mixed $diretorio
-     *
-     * @return void
-     */
     private function listFileFolder($diretorio) {
         $arq = array();
         foreach ($diretorio as $file) {
@@ -34,13 +27,6 @@ class Dir {
         return $arq;
     }
 
-    /**
-     * listFileOnly
-     *
-     * @param  mixed $diretorio
-     *
-     * @return void
-     */
     private function listFileOnly($diretorio) {
         $arq = array();
         foreach ($diretorio as $file) {
@@ -52,13 +38,6 @@ class Dir {
         return $arq;
     }
 
-    /**
-     * listFolderOnly
-     *
-     * @param  mixed $diretorio
-     *
-     * @return void
-     */
     private function listFolderOnly($diretorio) {
         $arq = array();
         foreach ($diretorio as $file) {
@@ -70,20 +49,12 @@ class Dir {
         return $arq;
     }
 
-    /**
-     * listFileFolderSubFolder
-     *
-     * @param  mixed $diretorio
-     * @param  mixed $diretorio_base
-     *
-     * @return void
-     */
     private function listFileFolderSubFolder($diretorio, $diretorio_base) {
-        $arq = array();
+        //$arq = array();
         foreach ($diretorio as $file) {
             if (!$file->isDot() && $file->isDir()) {
                 $dname = $file->getFilename();
-                $retorno = self::listMethod($diretorio_base . '/' . $dname, 2);
+                $retorno = $this->listMethod($diretorio_base . '/' . $dname, 2);
                 is_array($retorno) ? $arq[$dname] = $retorno : $arq[$dname] = array();
             }
         }
@@ -96,49 +67,37 @@ class Dir {
         return $arq;
     }
 
-    /**
-     * listType
-     *
-     * @param  mixed $diretorio
-     * @param  mixed $tipo
-     *
-     * @return void
-     */
     private function listType($diretorio, $tipo) {
         $arq = "";
-        if ($tipo == "LIST_ALL") {
+        if ($tipo == 3) {
             // Listo Pastas e Arquivos
-            $arq = self::listFileFolder($diretorio);
-        } else if ($tipo == "FILE_ONLY") {
+            $arq = $this->listFileFolder($diretorio);
+        } else if ($tipo == 1) {
             // Listo Apenas Arquivos
-            $arq = self::listFileOnly($diretorio);
-        } else if ($tipo == "FOLDER_ONLY") {
+            $arq = $this->listFileOnly($diretorio);
+        } else if ($tipo == 2) {
             // Listo Apenas Pastas
-            $arq = self::listFolderOnly($diretorio);
+            $arq = $this->listFolderOnly($diretorio);
         }
         return $arq;
     }
 
     /**
-     * $nivel = THIS_FOLDER (Lista apenas a pasta atual)
-     * $nivel = SUB_FOLDERS (Lista o diretorio atual + as sub pastas)
-     * 
-     * $tipo = FILE_ONLY (Lista apenas arquivos)
-     * $tipo = FOLDER_ONLY (Lista apenas pastas)
-     * $tipo = LIST_ALL (Lista os 2)
+     * $nivel = 1 (Lista apenas a pasta atual) $nivel = 2 (Lista o diretorio atual + as sub pastas)
+     * $tipo = 1 (Lista apenas arquivos) $tipo = 2 (Lista apenas pastas) $tipo = 3 (Lista os 2)
      * 
      * @param DirectoryIterator $diretorio_base
-     * @param string $nivel
-     * @param string $tipo
+     * @param int $nivel
+     * @param int $tipo
      * @return array
      */
-    public function listMethod($diretorio_base, $nivel = "THIS_FOLDER", $tipo = "LIST_ALL") {
+    public function listMethod($diretorio_base, $nivel = 1, $tipo = 3) {
         if (is_dir($diretorio_base)) {
             $diretorio = new DirectoryIterator($diretorio_base);
-            if ($nivel == "THIS_FOLDER") {
-                $arq = self::listType($diretorio, $tipo);
+            if ($nivel == 1) {
+                $arq = $this->listType($diretorio, $tipo);
             } else {
-                $arq = self::listFileFolderSubFolder($diretorio, $diretorio_base);
+                $arq = $this->listFileFolderSubFolder($diretorio, $diretorio_base);
             }
             $dir = $arq;
             return $dir;
